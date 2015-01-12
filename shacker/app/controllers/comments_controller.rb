d3a557c05acadacb9e4ae1412478e119b4942a61
DJDeWitt <djdewitt@gmail.com>
2015-01-12 15:13:44 -0600

    create skeleton and add posts

---
+++ shacker/app/controllers/comments_controller.rb
@@ -0,0 +1,5 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+
+end
---
+++ shacker/app/views/posts/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @post, :id, :url, :title, :created_at, :updated_at
